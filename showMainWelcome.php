c5f817a64c46f961f49554482b2c4fcf98cb2fa7
shaikio <stephen.haikio@gmail.com>
2019-03-04 17:39:26 -0500

    Add files via upload

---
+++ showMainWelcome.php
@@ -0,0 +1,10 @@
+<!-- 
+ this is a partial page
+ purpose is to show a welcome message under the top menu
+ even though the file extension is .php, all of the code on this page is html
+ -->
+ 
+<div class="form-container">
+<h1>Your Cookbook</h1>
+<p>Welcome to your personalized cookbook.</p>
+</div>
