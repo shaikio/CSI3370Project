4de5508deffb6c93d07946edda4cf077cf6100e3
shaikio <stephen.haikio@gmail.com>
2019-03-05 19:04:41 -0500

    Registration is working - Thank you Melvin!

--- register.php
+++ register.php
@@ -1,28 +1,27 @@
-<?php
-	
-	$username = "root";
-	$password = "";
-	$hostname = "localhost";
-	$dbname = "recipelists";
-	
-	$selected = mysqli_connect($hostname, $username, $password, $dbname);
-	
-	
-	if(isset($_POST['user_name']) && isset($_POST['user_password'])){
-		$user = $_POST['user_name'];
-		$pass = $_POST['user_password'];
+<<?php
+    $username = "root";
+    $password = "";
+    $hostname = "localhost";
+    $dbname = "recipelists";
 
-		$query = "SELECT * FROM 'users_table' WHERE user_name='$user'";
-		$result = mysqli_query($selected, $query);
-		if(mysqli_num_rows($result) > 0 ) { 
-			echo "User name already exists!";
-		}else{
-			$query = "INSERT INTO 'users_table' (user_name, user_password) VALUES ('$user', '$pass')";
-			$result = mysqli_query($selected, $query);
-			header("location:index.php");
-		}
-	}
-	mysqli_close($selected);
+	session_start();
 	
+    $selected = mysqli_connect($hostname, $username, $password, $dbname);
+
+    if(isset($_GET['user_name']) && isset($_GET['user_password'])){
+        $user = $_GET['user_name'];
+        $pass = $_GET['user_password'];
+        $query = "SELECT * FROM 'users_table' WHERE user_name='$user'";
+        $result = mysqli_query($selected, $query);
+        if(mysqli_num_rows($result) > 0 ) {
+            echo "User name already exists!";
+        }else{
+            $query = "INSERT INTO users_table (user_name, user_password) VALUES ('$user', '$pass')";
+            $result = mysqli_query($selected, $query);
+            header("location:index.php");
+        }
+    }
+    mysqli_close($selected);
+    
 ?>
 
